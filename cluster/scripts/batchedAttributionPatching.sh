6a45cb6fb5e3b6464c2c6b22244c333c14912ff2
NainaniJatinZ <nainani.jatin.0@gmail.com>
2024-05-13 18:52:22 +0000

    Prompt testing updates

--- cluster/scripts/batchedAttributionPatching.sh
+++ cluster/scripts/batchedAttributionPatching.sh
@@ -2,7 +2,7 @@
 #SBATCH  -t 3-00:00:00
 #SBATCH -p gpu-preempt
 #SBATCH -G 1
-#SBATCH --mem 256GB
+#SBATCH --mem 512GB
 #SBATCH --constraint a100-80g
 #SBATCH --output=cluster/logs/infoRet.out
 #SBATCH --error=cluster/logs/infoRet.err
@@ -13,4 +13,4 @@ echo "Activated virtual environment"
 cd /home/jnainani_umass_edu/codellm/MechInterpCodeLLMs
 echo "Changed the directory to: ${PWD}"
 
-python -u seqAttPatching.py --logit_difference 0.9817 --NumberOfPrompts 4
+python -u seqAttPatching.py --logit_difference 0.5171 --NumberOfPrompts 8
